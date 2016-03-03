fa8570b6d44335bee21a54245477965bc7ac12ea
Mark Paluch <mpaluch@pivotal.io>
2016-03-03 08:42:05 +0100

    Use the @RedisHash annotation instead of @KeySpace.
    Redis Repositories use @RedisHash to identify the repository belongs to the Redis module and to create the appropriate repository type. We now use the @RedisHash annotation instead of the previous @KeySpace annotation.

--- 14-redis-repositories/src/main/java/example/Person.java
+++ 14-redis-repositories/src/main/java/example/Person.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Reference;
-import org.springframework.data.keyvalue.annotation.KeySpace;
+import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
 import lombok.Data;
 
 @Data
-@KeySpace("persons")
+@RedisHash("persons")
 class Person {
 
 	@Id String id;
